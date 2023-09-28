5057d9988daff53938a47790562fd4b9648db290
aditi018 <dasaditi018@gmail.com>
2023-09-28 23:01:30 +0530

    Added nearbyJobs component

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -13,8 +13,6 @@ const Nearbyjobs = () => {
     num_pages :1
   })
 
-  console.log(data);
-
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -29,13 +27,13 @@ const Nearbyjobs = () => {
         ) : error ? (
           <Text>Something went wrong</Text>
         ): (
-          data?.map((job)=>{
+          data?.map((job)=>(
             <NearbyJobCard
             job={job}
             key={`nearby-job-${job.job_id}`} 
             handleNavigate={()=> router.push(`/job-details/${job.job_id}`)}
             />
-          })
+          ))
         )}
       </View>
     </View>
